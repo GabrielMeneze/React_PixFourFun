f45904e4aa0a45ecd9936d223161d1848d5a3ed3
João Pedro <jpedrodss@gmail.com>
2020-12-01 13:07:37 -0300

    Banner da home incluido

---
+++ pix4fun/src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+export default function Home() {
+    return (
+        <div>
+            <div className="mainBanner">
+                <h1>IMPRIMA SUAS MEMÓRIAS COM ESTILO</h1>
+                <button className="btnMainBanner">
+                    <p>CONHEÇA</p>
+                </button>
+            </div>
+        </div>
+    )
+}
