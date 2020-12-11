aacc6dafdd2b6f4305a829718685caa2ba040a1b
GabrielMeneze <65770195+GabrielMeneze@users.noreply.github.com>
2020-12-11 02:40:54 -0300

    upload imagem funcionando

--- pix4fun/src/pages/UploadImagem/index.jsx
+++ pix4fun/src/pages/UploadImagem/index.jsx
@@ -1,37 +1,63 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import Header from '../../components/Header/index';
 import Footer from '../../components/Footer/index';
 import './index.css';
 
 
+const UploadImagem = () => { 
 
-const UploadImagem = () => {
+  const [image, setImage] = useState('')
+  const [loading, setLoading] = useState('')
+
+  const UploadImage = async e =>{ 
+    const files = e.target.files
+    const data = new FormData()
+    data.append('file', files[0])
+    setLoading(true)
+    const res = await fetch('http://localhost:3000/upload/imagens/',
+    {
+      method : 'POST',
+      body : data
+    })
+    const file = await res.json()
+
+    setImage(file.secure_url)
+    setLoading(false)
+  }
+  
+
   return (
     <div className="Main">
-      <div>
-        <Header />
-      </div>
+    
+
+        <Header/>
 
       <div className="ContainerText">
         <h2 className="text">
           Agora é a hora de<br></br> nos enviar suas<br></br> fotos!
           <span className="textGray">Simples e <br></br>rápido.</span>
-
           <h3>Selecione suas fotos favoritas, recorte<br></br> no formato em que deseja e clique e<br></br> enviar. Pronto! Receberemos um aviso e<br></br> suas fotos começarão a ser impressas.
           </h3>
         </h2>
+      </div>
+      
+        <div className="Up">
+          <input
+            type="file"
+            name="file"          
+            placeholder="Upload an image"
+            onChange={UploadImage}
+          />
 
-        <div className="botton">
-          <p>Escolher Fotos</p>
+          {loading ? (
+            <h3>loading...</h3>
+          ): ( 
+            <img src={image} style={{width: '300px'}} />
+          )}
         </div>
-      </div>
 
-
-      <div>
-        <Footer />
-      </div>
-    </div>
+        <Footer/>
+    </div> 
   )
 }
-
 export default UploadImagem;
