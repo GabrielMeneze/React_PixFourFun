4dd0c0b13291ba58870c0f7190021ed2cd4354e7
GabrielMeneze <65770195+GabrielMeneze@users.noreply.github.com>
2020-12-15 05:16:35 -0300

    crop iniciado

--- pix4fun/src/pages/UploadImagem/index.jsx
+++ pix4fun/src/pages/UploadImagem/index.jsx
@@ -1,11 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Header from '../../components/Header/index';
 import Footer from '../../components/Footer/index';
-import {url} from '../../utils/constants'
+import ReactDOM from 'react-dom'
+import Cropper from 'react-easy-crop'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+import { getOrientation } from 'get-orientation/browser'
 import './index.css';
 
 
-const UploadImagem = () => {
+const ORIENTATION_TO_ANGLE = {
+  '3': 180,
+  '6': 90,
+  '8': -90,
+}
+
+const UploadImagem = ({classes}) => {
+
+// Parte teste crop---------------------------------------------------------------------------------------
+const [imageSrc, setImageSrc] = React.useState(null)
+  const [crop, setCrop] = useState({ x: 0, y: 0 })
+  const [rotation, setRotation] = useState(0)
+  const [zoom, setZoom] = useState(1)
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
+  const [croppedImage, setCroppedImage] = useState(null)
+
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    setCroppedAreaPixels(croppedAreaPixels)
+  }, [])
+
+  const showCroppedImage = useCallback(async () => {
+    try {
+      const croppedImage = await getCroppedImg(
+        imageSrc,
+        croppedAreaPixels,
+        rotation
+      )
+      console.log('donee', { croppedImage })
+      setCroppedImage(croppedImage)
+    } catch (e) {
+      console.error(e)
+    }
+  }, [imageSrc, croppedAreaPixels, rotation])
+
+  const onClose = useCallback(() => {
+    setCroppedImage(null)
+  }, [])
+
+  const onFileChange = async e => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0]
+      let imageDataUrl = await readFile(file)
+
+      // apply rotation if needed
+      const orientation = await getOrientation(file)
+      const rotation = ORIENTATION_TO_ANGLE[orientation]
+      if (rotation) {
+        imageDataUrl = await getRotatedImage(imageDataUrl, rotation)
+      }
+
+      setImageSrc(imageDataUrl)
+    }
+  }
+
+
+
+
+
+
+
+
   // Parte teste listar--------------------------------------------------------------------------
   const [token, setToken] = useState('')
   const [texto, setTexto] = useState('');
@@ -75,8 +140,82 @@ const escolherImg = event => {
   }
 
   return (
+
+    // Parte do crop
+    
+
+    
     <div className="ContainerMain">
       <Header />
+      
+      <div>
+      {imageSrc ? (
+        <React.Fragment>
+          <div className={classes.cropContainer}>
+            <Cropper
+              image={imageSrc}
+              crop={crop}
+              rotation={rotation}
+              zoom={zoom}
+              aspect={4 / 3}
+              onCropChange={setCrop}
+              onRotationChange={setRotation}
+              onCropComplete={onCropComplete}
+              onZoomChange={setZoom}
+            />
+          </div>
+          <div className={classes.controls}>
+            <div className={classes.sliderContainer}>
+              <Typography
+                variant="overline"
+                classes={{ root: classes.sliderLabel }}
+              >
+                Zoom
+              </Typography>
+              <Slider
+                value={zoom}
+                min={1}
+                max={3}
+                step={0.1}
+                aria-labelledby="Zoom"
+                classes={{ container: classes.slider }}
+                onChange={(e, zoom) => setZoom(zoom)}
+              />
+            </div>
+            <div className={classes.sliderContainer}>
+              <Typography
+                variant="overline"
+                classes={{ root: classes.sliderLabel }}
+              >
+                Rotation
+              </Typography>
+              <Slider
+                value={rotation}
+                min={0}
+                max={360}
+                step={1}
+                aria-labelledby="Rotation"
+                classes={{ container: classes.slider }}
+                onChange={(e, rotation) => setRotation(rotation)}
+              />
+            </div>
+            <Button
+              onClick={showCroppedImage}
+              variant="contained"
+              color="primary"
+              classes={{ root: classes.cropButton }}
+            >
+              Show Result
+            </Button>
+          </div>
+          <ImgDialog img={croppedImage} onClose={onClose} />
+        </React.Fragment>
+      ) : (
+        <input type="file" onChange={onFileChange} accept="image/*" />
+      )}
+    </div>
+
+
 
       {/* Containem que contem a parte de escolha do usuario */}
       <div className="ContainerOne">
