05fa46c438636f8c7642753b33ea8f8c7d991c67
João Pedro <jpedrodss@gmail.com>
2020-12-19 17:27:21 -0300

    implementação do correios com erro (connection refused)

--- pix4fun/src/pages/Pagamento/index.jsx
+++ pix4fun/src/pages/Pagamento/index.jsx
@@ -2,51 +2,68 @@ import React, { useState } from 'react';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import { Button, Form } from 'react-bootstrap';
-import XMLParser from 'react-xml-parser';
 
 export default function Pagamento() {
-    const [cepDestino, setCepDestino] = useState('');
-
-    const { calcularPrecoPrazo } = require("correios-brasil");
+    let Correios = require('node-correios');
+    let correios = new Correios();
 
     let args = {
         sCepOrigem: "81200100",
-        sCepDestino: cepDestino,
+        sCepDestino: "05205380",
         nVlPeso: ".1",
         nCdFormato: "3",
         nVlComprimento: "15",
         nVlAltura: "0",
         nVlLargura: "15",
-        nCdServico: ["04014", "04510"],
+        nCdServico: "04014",
         nVlDiametro: "0"
     };
 
-    const calcularCep = (event) => {
-        event.preventDefault()
-
-        fetch(calcularPrecoPrazo(args), {
-            mode: "cors",
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Origin, X-Request-Width, Content-Type, Accept'
-            },
-            body: JSON.stringify(args)
+    fetch(correios.calcPreco(args), {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, X-Request-Width, Content-Type, Accept'
+        }
+    })
+        .then(result => {
+            console.log(result);
         })
-            .then((data) => {
-                console.log(data)
-                var xml = new XMLParser().parseFromString(data);
-                console.log(xml)
-            })
-            .then((response) => {
-                console.log(response.json())
-            })
-    }
+        .catch(error => {
+            console.log(error);
+        });
 
-    // calcularPrecoPrazo(args).then((response) => {
-    //     console.log(response.json());
-    //   });
 
-    // SDK de Mercado Pago
+    // const [cepDestino, setCepDestino] = useState('');
+
+    // const { calcularPrecoPrazo } = require("correios-brasil");
+
+    // let args = {
+    //     sCepOrigem: "81200100",
+    //     sCepDestino: cepDestino,
+    //     nVlPeso: ".1",
+    //     nCdFormato: "3",
+    //     nVlComprimento: "15",
+    //     nVlAltura: "0",
+    //     nVlLargura: "15",
+    //     nCdServico: "04014",
+    //     nVlDiametro: "0"
+    // };
+
+    // const calcularCep = (event) => {
+    //     event.preventDefault()
+
+    //     fetch(calcularPrecoPrazo(args), {
+    //         headers: {
+    //             'Access-Control-Allow-Origin': '*',
+    //             'Access-Control-Allow-Headers': 'Origin, X-Request-Width, Content-Type, Accept'
+    //         }
+    //     })
+    //         .then((data) => {
+    //             console.log(data)
+    //         })
+    // }
+
+    // // SDK de Mercado Pago
     // const mercadopago = require('mercadopago');
 
     // // Configura credenciais
@@ -76,18 +93,18 @@ export default function Pagamento() {
     return (
         <div>
             <Header />
-            <Form onSubmit={event => calcularCep(event)}>
+            {/* <Form onSubmit={event => calcularCep(event)}>
                 <Form.Group>
                     <Form.Label>Digite seu CEP</Form.Label>
                     <Form.Control value={cepDestino} onChange={event => setCepDestino(event.target.value)} type="text" placeholder="Digite aqui seu CEP" />
                     <Button type="submit">Calcular</Button>
                 </Form.Group>
-            </Form>
+            </Form> */}
 
-            {/* <script
+            <script
                 src="https://www.mercadopago.com.br/integrations/v1/web-payment-checkout.js"
                 data-preference-id='<%= global.id %>'>
-            </script> */}
+            </script>
             <Footer />
         </div>
     )
